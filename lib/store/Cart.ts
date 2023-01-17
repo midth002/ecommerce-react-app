1eb46de61c186f5837b01904cfb2aa89053009a8
Andrew Midthun <midth002@gmail.com>
2023-01-17 12:25:31 -0600

    Add: updated cart.ts file with persist middleware for zustand.

--- lib/store/Cart.ts
+++ lib/store/Cart.ts
@@ -27,3 +27,40 @@ export interface CartStoreProps {
     resetCart: () => void;
 }
 
+const useCartStore = create<CartStoreProps>()(
+    persist(
+      (set) => ({
+        cart: defaultState,
+        addToCart: (item: ProductType) =>
+          set((state) => ({
+            cart: {
+              items: [...state.cart.items, item]
+            }
+          })),
+        removeFromCart: (item: ProductType) =>
+          set((state) => ({
+            cart: {
+              items: state.cart.items.filter((i: ProductType) => item.id !== i.id)
+            }
+          })),
+        updateCartItem: (item: ProductType, cartItemIndex: number) =>
+          set((state) => {
+            const cartItems = [...state.cart.items];
+            cartItems[cartItemIndex] = item;
+            return {
+              cart: {
+                items: cartItems
+              }
+            };
+          }),
+        resetCart: () =>
+          set(() => ({
+            cart: defaultState
+          }))
+      }),
+      { name: "cart-store" }
+    )
+  );
+
+
+export default useCartStore;
