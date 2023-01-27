92015c00f36f8636c8b10f7a53e0165ce18dd5b4
Andrew Midthun <midth002@gmail.com>
2023-01-27 11:33:34 -0600

    Feature: Added total cart price on checkout page.

--- components/cart/cartPage/CartPage.tsx
+++ components/cart/cartPage/CartPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useCartStore } from '../../../lib/store';
+import { usePriceFormatter } from '../../../lib/hooks/usePriceFormatter';
 import { CartItem } from '../../item';
 import { Box, Typography, Button } from '@mui/material';
 
@@ -7,15 +8,23 @@ const CartPage = () => {
 
     const { cart, updateCartItem, removeFromCart } = useCartStore();
 
+    
+    const calcTotalPrice = (items: any) => {
+      const totalPrice =  items.reduce((total: any, item: any)=> total + item.price,0)
+      const formattedUnitPrice = usePriceFormatter.format(totalPrice / 100);
+     return formattedUnitPrice;
+    }
+    
     console.log(cart);
 
   return (
-    <Box sx={{boxShadow: 3, 
+    <Box sx={{ 
       m: 5,
       width: '75%'
     }}>
       <Typography variant='h2'>Your Cart</Typography>
-      
+
+      <Typography>Total: {calcTotalPrice(cart.items)}</Typography>
       {cart.items.length >= 1 &&
         cart.items.map((cart: any, index: number) => (
           <Box key={cart.id}>
